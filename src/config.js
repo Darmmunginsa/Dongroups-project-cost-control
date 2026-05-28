30731060beb4ae1f3135c66cc4d73cb5447593cd
Darm <darmmunginsa@gmail.com>
2026-05-28 22:16:47 +0700

    fix: update Apps Script URL

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 //  แก้ไข APPS_SCRIPT_URL ให้ตรงกับ Web App URL ที่ Deploy จาก
 //  Google Apps Script ของคุณ
 // ============================================================
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx6Pwnc4icP1-U7tBqvuC_1At53m3paR17fNAm0Zewzp962lO-osfD8qMla3U76l_YH/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz5v8Q1PRrFT1WeuvPeASS4LIIb8yBpWCaZMj50ImbhHjxypmHaO0IOYpnEqUp2N6FOzg/exec';
